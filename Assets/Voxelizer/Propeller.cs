2c379e886c0dfc8524fdc770770bb2809c198297
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-04-14 11:12:57 +0900

    テスト用の回転変換クラス

---
+++ Assets/Voxelizer/Propeller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Propeller : MonoBehaviour {
+	public const float ROUND = 360f;
+	public const double TICKToSecond = 1e-7;
+	public Vector3 timeToAngle;
+
+	System.DateTime startTime;
+
+	void OnEnable() {
+		startTime = System.DateTime.Now;
+	}
+	void Update() {
+		var seconds = (float)((System.DateTime.Now - startTime).Ticks * TICKToSecond);
+		transform.localRotation = Quaternion.Euler (seconds * ROUND * timeToAngle);
+	}
+}
