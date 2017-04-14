3eaad46e0b2ac05df4fa9bb1fdebda716afca2b7
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-04-14 14:23:05 +0900

    Voxel のクリア処理クラス

---
+++ Assets/Voxelizer/VoxelTextureCleaner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelTextureCleaner {
+	public const string RESULT = "Result";
+
+	protected readonly ComputeShader clear;
+	protected readonly int kernelId;
+	protected readonly int textureId;
+
+	protected readonly uint x, y, z;
+
+	public VoxelTextureCleaner(ComputeShader clear, int kernelId, int textureId) {
+		this.clear = clear;
+		this.kernelId = kernelId;
+		this.textureId = textureId;
+
+		clear.GetKernelThreadGroupSizes (kernelId, out x, out y, out z);
+	}
+
+	public void Clear (RenderTexture tex) {
+		clear.SetTexture (0, textureId, tex);
+		clear.Dispatch (0, tex.width / (int)x, tex.height / (int)y, tex.depth / (int)z);
+	}
+}
