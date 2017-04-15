3de1cdb62f4ce51e5e23b1d64c875337c1de2f60
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-04-16 01:11:38 +0900

    Gist のカメラクラスを使ってみる

--- Assets/Voxelizer/FrustumVoxelizer.cs
+++ Assets/Voxelizer/FrustumVoxelizer.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Gist;
 
 [ExecuteInEditMode]
 public class FrustumVoxelizer : MonoBehaviour {
+	public const float NEAR_DISTANCE = 10f;
+
 	public const string RESULT = "Result";
 	public const string VOXEL_SIZE = "_VoxelSize";
 	public const string VOXEL_COLOR_TEX = "_VoxelColorTex";
@@ -14,6 +17,9 @@ public class FrustumVoxelizer : MonoBehaviour {
 	int PROP_VOXEL_FACE_TEX;
 
 	public TextureEvent OnCreateVoxelTexture;
+	
+	public Bounds bounds = new Bounds (Vector3.zero, 10f * Vector3.one);
+	public Color boundsColor = Color.green;
 
 	public int prefferedVoxelResolution = 512;
 	public FilterMode voxelFilterMode = FilterMode.Bilinear;
@@ -21,10 +27,11 @@ public class FrustumVoxelizer : MonoBehaviour {
 	public ComputeShader clearCompute;
 	public Shader voxelShader;
 
-	Camera targetCam;
 	VoxelTexture colorTex;
 	VoxelTexture faceTex;
 	VoxelTextureCleaner cleaner;
+
+	ManuallyRenderCamera renderCam;
 
 	#region Unity
 	void OnEnable() {
@@ -32,14 +39,22 @@ public class FrustumVoxelizer : MonoBehaviour {
 		PROP_VOXEL_COLOR_TEX = Shader.PropertyToID(VOXEL_COLOR_TEX);
 		PROP_VOXEL_FACE_TEX = Shader.PropertyToID (VOXEL_FACE_TEX);
 
-		targetCam = GetComponent<Camera> ();
-		targetCam.enabled = false;
+		renderCam = new ManuallyRenderCamera ((cam) => {
+			var bextent = bounds.extents;
+			cam.transform.position = transform.position - (bextent.z + NEAR_DISTANCE) * transform.forward;
+			cam.transform.rotation = transform.rotation;
+
+			var bsize = bounds.size;
+			cam.orthographic = true;
+			cam.orthographicSize = bextent.y;
+			cam.nearClipPlane = NEAR_DISTANCE;
+			cam.farClipPlane = bsize.z + NEAR_DISTANCE;
+			cam.aspect = bsize.x / bsize.y;
+		});
 
 		colorTex = new VoxelTexture (prefferedVoxelResolution, RenderTextureFormat.ARGB32);
 		colorTex.OnCreateVoxelTexture += (v) => {
 			OnCreateVoxelTexture.Invoke(v.Texture);
-			ReleaseCameraTargetTexture();
-			CreateCameraTargetTexture();
 		};
 
 		faceTex = new VoxelTexture (prefferedVoxelResolution, RenderTextureFormat.R8);
@@ -56,9 +71,18 @@ public class FrustumVoxelizer : MonoBehaviour {
 		Shader.SetGlobalTexture (PROP_VOXEL_FACE_TEX, faceTex.Texture);
 		Render();
 	}
+	void OnDrawGizmos() {
+		if (!isActiveAndEnabled)
+			return;
+
+		Gizmos.color = boundsColor;
+		Gizmos.matrix = transform.localToWorldMatrix;
+		Gizmos.DrawWireCube (bounds.center, bounds.size);
+		Gizmos.matrix = Matrix4x4.identity;
+	}
 	void OnDisable() {
 		colorTex.Dispose ();
-		ReleaseCameraTargetTexture ();
+		renderCam.Dispose ();
 	}
 	#endregion
 
@@ -71,25 +95,20 @@ public class FrustumVoxelizer : MonoBehaviour {
 		cleaner.Clear (faceTex.Texture);
 	}
 	void Render () {
-		Graphics.SetRandomWriteTarget (1, colorTex.Texture);
-        Graphics.SetRandomWriteTarget (2, faceTex.Texture);
-		targetCam.RenderWithShader (voxelShader, null);
-		Graphics.ClearRandomWriteTargets ();
+		var targetTex = RenderTexture.GetTemporary (colorTex.CurrentResolution, colorTex.CurrentResolution);
+		try {
+			Graphics.SetRandomWriteTarget (1, colorTex.Texture);
+	        Graphics.SetRandomWriteTarget (2, faceTex.Texture);
+			renderCam.RenderWithShader (targetTex, voxelShader, null);
+			Graphics.ClearRandomWriteTargets ();
+		} finally {
+			RenderTexture.ReleaseTemporary (targetTex);
+		}
 	}
 	void Release<T>(ref T resource) where T : Object {
 		if (Application.isPlaying)
 			Object.Destroy (resource);
 		else
 			Object.DestroyImmediate (resource);
-	}
-
-	void ReleaseCameraTargetTexture () {
-		var tex = targetCam.targetTexture;
-		targetCam.targetTexture = null;
-		RenderTexture.ReleaseTemporary (tex);
-	}
-	void CreateCameraTargetTexture() {
-		var w = colorTex.CurrentResolution;
-		targetCam.targetTexture = RenderTexture.GetTemporary (w, w, 0);
 	}
 }
